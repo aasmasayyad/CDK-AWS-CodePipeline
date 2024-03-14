75b8aad5e05c7ab9a938f3510211c68f6e0b8047
Aasma Sayyad <aasma@rizmax.com>
2024-03-14 08:07:46 -0400

    second commit

--- bin/cdk-aws-code_pipeline.ts
+++ bin/cdk-aws-code_pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkAwsCodePipelineStack } from '../lib/cdk-aws-code_pipeline-stack';
+import { CdkCicdStack } from '../lib/cdk-aws-code_pipeline-stack';
 
 const app = new cdk.App();
-new CdkAwsCodePipelineStack(app, 'CdkAwsCodePipelineStack', {
+new CdkCicdStack(app, 'Cdk-AwsCodePipelineStack-new', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
